1bcdcc4b106454965f71d840d42af5430b9a293c
yadaoleechongnor <135152671+yadaoleechongnor@users.noreply.github.com>
2024-02-24 23:09:07 +0700

    new

--- src/components/sidebarcomponents/Personcomponent.jsx
+++ src/components/sidebarcomponents/Personcomponent.jsx
@@ -1,57 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
-import profile from '../../assets/EagleDev99.svg';
 
 function PersonComponent() {
-  const [selectedPerson, setSelectedPerson] = useState(null);
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null); // Add state for error handling
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Provided user token
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQ5YWRkOTMyNWM4YTBlMTdmZDM5NjkiLCJpYXQiOjE3MDg3NjQ2MzMsImV4cCI6MTcwODc3MTgzM30.JZKqbVUV66VSdi3i9Uzu-gzmYamT0bbjHML1ddssw7w';
+    const fetchUsers = async () => {
+      try {
+        const API_URL = 'https://eaglechat.onrender.com/user/getUsers';
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWRhMDk1MDI3NDBmYWVhZDgyYWFiMjQiLCJpYXQiOjE3MDg3ODgwNDgsImV4cCI6MTcwODc5NTI0OH0.4LDViGEybc0O_ycf5ycXbFtLED36l6_uJVh8V2EnBjg';
+        
+        const response = await axios.get(API_URL, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
 
-    // Setting up Axios request headers with the token
-    const config = {
-      headers: {
-        'Authorization': `Bearer ${token}`
+        setUsers(response.data);
+      } catch (error) {
+        if (error.response && error.response.status === 403) {
+          setError('You are not authorized to access this resource.');
+        } else {
+          setError('An error occurred while fetching users. Please try again later.');
+        }
+        console.error('Error fetching users:', error);
       }
     };
 
-    axios.get('https://eaglechat.onrender.com/user/getUsers', config)
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching users:', error);
-        console.error('Response status:', error.response.status);
-        console.error('Response data:', error.response.data);
-        setError(error); // Set error state
-      });
+    fetchUsers();
   }, []);
-
-  const handlePersonClick = (userId) => {
-    setSelectedPerson(userId === selectedPerson ? null : userId);
-  };
-
-  const getShadowStyle = (userId) => {
-    return selectedPerson === userId
-      ? ` shadow-lg border`
-      : ` hover:shadow-lg hover:border `;
-  };
 
   return (
     <div className='w-full'>
-      {error && <div>Error: {error.message}</div>} {/* Display error message */}
+      {error && <div>Error: {error}</div>}
       {users.map((user) => (
         <NavLink to="/chat"
           key={user._id} 
-          className={`flex justify-between ${getShadowStyle(user._id)} rounded-lg items-center  font-medium `}
-          onClick={() => handlePersonClick(user._id)}
+          className={`flex justify-between ${user._id} rounded-lg items-center  font-medium `}
+          onClick={() => (user._id)}
         >
           <div className='flex items-center gap-3'>
-            <img src={profile} alt="" className='my-2 ml-2 rounded-full w-14 h-14' />
+            <img src={user.profilePicture} alt="" className='my-2 ml-2 rounded-full w-14 h-14' />
             <div>
               <span>{user.username}</span>
               <p className='text-sm text-gray-300'>you: ok </p>
